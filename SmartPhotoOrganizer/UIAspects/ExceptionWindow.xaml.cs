8096ac1fc82eb21e2072e2bdec1f430b494cf62a
Nizwer Hussain <nizwer@live.com>
2016-09-07 11:15:10 +0530

    v2

---
+++ SmartPhotoOrganizer/UIAspects/ExceptionWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SmartPhotoOrganizer.UIAspects
+{
+    /// <summary>
+    /// Interaction logic for ExceptionWindow.xaml
+    /// </summary>
+    public partial class ExceptionWindow
+    {
+        public ExceptionWindow(Exception exception)
+        {
+            InitializeComponent();
+
+            ExceptionTextBox.Text = exception + Environment.NewLine;
+        }
+    }
+}
---
+++ SmartPhotoOrganizer/UIAspects/Hello.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace SmartPhotoOrganizer.UIAspects
+{
+    /// <summary>
+    /// Interaction logic for Hello.xaml
+    /// </summary>
+    public partial class Hello
+    {
+        public Hello()
+        {
+            InitializeComponent();
+        }
+
+        public DirectorySet SourceDirectories => MultiFolderPicker.DirectorySet;
+
+        private void okButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MultiFolderPicker.HasDirectories)
+            {
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Please select at least a single folder");
+            }
+        }
+    }
+}
